e87e24b4087981dec76db05fe4aea3d0f8f77f0b
w2cluster227@iiht.tech <w2cluster227@iiht.tech>
2020-06-03 21:53:44 +0530

    before dockerize

---
+++ ConfigServer/src/test/java/com/ibm/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
