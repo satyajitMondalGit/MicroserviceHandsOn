3a0ed186ba86065506730fce6c37d9bd32e25ec5
w2cluster227 <w2cluster227@w2cluster268.iiht.tech>
2020-06-08 22:04:27 +0530

    dd

---
+++ LoginEurekaServer/src/main/java/com/ibm/eureka/server/LoginEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginEurekaServerApplication.class, args);
+	}
+
+}
---
+++ LoginEurekaServer/src/test/java/com/ibm/eureka/server/LoginEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
