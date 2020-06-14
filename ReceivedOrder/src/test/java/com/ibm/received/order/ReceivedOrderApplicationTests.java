19705010ebd3ffb4430cb24a6585dbe4b40870a2
w2cluster227 <w2cluster227@w2cluster268.iiht.tech>
2020-06-14 20:51:24 +0530

    2nd commit

---
+++ ReceivedOrder/src/test/java/com/ibm/received/order/ReceivedOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.received.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceivedOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
