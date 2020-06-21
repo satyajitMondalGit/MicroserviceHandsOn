2d4417e861d392ee1378623d56bf8d263fe2e8e2
SatyajitMondal <SatyajitMondal@DESKTOP-AB6IV03>
2020-06-21 12:49:16 +0530

    commit n

---
+++ StoreApiGateway/src/test/java/com/ibm/store/api/gateway/StoreApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.store.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
