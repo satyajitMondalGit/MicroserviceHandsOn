07e22f3d09cade4621ba98484878f1f6203b08a1
SatyajitMondal <SatyajitMondal@9.85.87.219>
2020-06-25 15:22:42 +0530

    commit

---
+++ OAuthResourceServer/src/test/java/com/ibm/oauth/resource/server/OAuthResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.oauth.resource.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
